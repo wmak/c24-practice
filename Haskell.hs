ef08df727a2ade971905a721519919ca141aa88c
wmak <william.mak@hotmail.ca>
2014-04-16 13:09:45 -0400

    finished more haskell

--- Haskell.hs
+++ Haskell.hs
@@ -1,5 +1,25 @@
 module Haskell where
 
+{- Write a procedure that, given a list of non-empty lists, returns the list of
+ - ﬁrst elements of the sub-lists -}
+
+{- Use map -}
+one l = map head l
+one :: [[a]] -> [a]
+
+{- Use fold -}
+two l = foldr (\x y -> take 1 x ++ y) [] l
+two :: [[a]] -> [a]
+
+{- Write a procedure that returns the sum of all elements in the input list. -}
+
+{- Use fold -}
+three l = foldr (\x y -> x + y) 0 l
+three :: [Int] -> Int
+
+{- Use apply -}
+{- Not applicable in Haskell -}
+
 {- Deﬁne a function double which returns [2*a0, 2*a1, 2*a2, ...] when called
  - with an input list
  -
@@ -7,3 +27,10 @@ module Haskell where
  -}
 double l = [2*x | x <- l]
 double :: [Double] -> [Double]
+
+{- Deﬁne the function expTaylor x which returns a list, so that summing this
+ - list gives the above series. -}
+expTaylor x = [x**i/(factorial i) | i <- [1..]]
+expTaylor :: Double -> [Double]
+
+factorial n = if n == 0 then 1 else n * factorial (n - 1)
